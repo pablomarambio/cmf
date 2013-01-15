eafc6e83ddb74013602714fcd578264708a7b20c
Pablo Marambio <pablo@pablomarambio.cl>
2013-01-14 21:51:05 -0300

    Proyecto creado

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Cmf::Application.config.secret_token = '83c406d57946dfc675a5c028c394e7595498d6e2adb8506c731aba98e811981610b2f06030d5cf7ce1112eb525e7c6f32facf4e4e909323f40c2d6d8a6658f09'
