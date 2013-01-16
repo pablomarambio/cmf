162df2e5ae4aae0ea5cc4d63e6b8ab62e0e48c08
Pablo Marambio <pablo@pablomarambio.cl>
2013-01-16 13:06:13 -0300

    Nuevas llaves para twitter

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -4,5 +4,5 @@ Rails.application.config.middleware.use OmniAuth::Builder do
   provider :facebook, '402883789797877', '3fe235e5d8e212371b21a1b6a17e8a79' if Rails.env.development?
   provider :facebook, '150544564969139', '0975c80e44d6d10ed6936e8600bdf438' if Rails.env.production?
 
-  provider :twitter, 'aI6KYrW35M8IAk2wnzlA', 'rMkMXMuhh5YFKuTKXY0M235N320XtwOW2DMx662Vw2Q'
+  provider :twitter, 'PQEPJB8yLrsgvXpD8vxOaA', 'DppfJtzvgzD98rRWhV7gS2FuI6TIt8mJesFODncw4ro'
 end
