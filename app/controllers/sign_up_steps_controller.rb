953a643bee7cf5728c4d41a4582a73bdad93c4a4
Sebastián Machuca Sáez <sams91@gmail.com>
2013-01-30 20:58:58 -0300

    Validaciones de modelo User en los distintos pasos del Wizard. Hacer RAKE MIGRATE.

--- app/controllers/sign_up_steps_controller.rb
+++ app/controllers/sign_up_steps_controller.rb
@@ -1,14 +1,28 @@
 class SignUpStepsController < ApplicationController
   include Wicked::Wizard
-  before_filter :find_user
+  before_filter :find_user, :except => [:create]
   steps :set_email, :set_social_networks, :set_statement
 
   def show
     render_wizard 
   end
 
+  def create
+    @user = User.new(params[:user])
+    @user.status = "threshold"
+    if @user.save
+      puts @user.inspect
+      session[:user_id] = @user.id
+      redirect_to wizard_path(steps.first, :user_id => @user.id)
+    else
+      render "users/new"
+    end
+  end
+
   def update
-    @user.attributes = params[:user]
+    params[:user][:status] = step.to_s
+    params[:user][:status] = 'active' if step == steps.last
+    @user.update_attributes(params[:user])
     render_wizard @user
   end
 
--- app/models/user.rb
+++ app/models/user.rb
@@ -7,7 +7,37 @@ class User < ActiveRecord::Base
 
   # Setup accessible (or protected) attributes for your model
   attr_accessible :role_ids, :as => :admin
-  attr_accessible :name, :email, :password, :password_confirmation, :remember_me, :haslocalpw, :comment, :threshold
+  attr_accessible :name, :email, :password, :password_confirmation, :remember_me, :haslocalpw, :comment, :threshold, :status
 
   has_many :auth_providers, :dependent => :destroy
+
+  validates :threshold, :numericality => true, :if => :active_or_threshold?
+
+  validates :email, :uniqueness => true, :presence => true, :format => {:with => /(?>(?:[0-9a-zA-Z][-\w]*[0-9a-zA-Z]\.)+)[a-zA-Z]{2,9}/}, :if => :active_or_email?
+
+  validates :comment, :presence => true, :if => :active_or_comment?
+
+  validates :name, :presence => true, :if => :active_or_name?
+
+  def active?
+    status == 'active'
+  end
+  
+  def active_or_threshold?
+    status.include?('threshold') || active?
+  end
+
+  def active_or_name?
+    status.include?('set_statement') || active?
+  end
+  
+  def active_or_email?
+    status.include?('set_email') || active?
+  end
+  
+  def active_or_comment?
+    status.include?('set_statement') || active?
+  end
+  
+
 end
