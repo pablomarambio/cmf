85eaa9eba3550fd12e183228993e8409d48f77de
Pablo Marambio <pablo@pablomarambio.cl>
2013-03-23 09:54:36 -0300

    Ahora se admiten usuarios con puntos y caracteres raros

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Cmf::Application.routes.draw do
   resources :answers, :except => :new
-  get "answers/:username/:token" => "answers#new", :as => "new_answer"
+  get "answers/:username/:token" => "answers#new", :as => "new_answer", :constraints  => { :username => /[\w_\-\.]*/}
   get "evaluate/:id/:token" => "answers#evaluate_answer", :as => "evaluate_answer"
   put "set_evaluation/:id" => "answers#set_evaluation", :as => "set_evaluation"
   post "payment/create" => "payments#create", :as => "new_payment"
@@ -9,14 +9,14 @@ Cmf::Application.routes.draw do
   get "users_profile" => "home#users_profile", :as => "users_profile"
 
   resources :messages, :except => [:new]
-  get "message/new/:username/:payment_id/:payment_random" => "messages#new", :as => "new_message"
+  get "message/new/:username/:payment_id/:payment_random" => "messages#new", :as => "new_message", :constraints  => { :username => /[\w_\-\.]*/}
 
 
   match "auth/:provider/callback" => "sign_up_steps#auth_provider_create"
   resources :auth_providers, :only => [:index, :create, :destroy]
 
   get "profile" => "home#profile"
-  get "profile/:username" => "home#public_profile"
+  get "profile/:username" => "home#public_profile", :constraints  => { :username => /[\w_\-\.]*/}
 
   resources :sign_up_steps
   authenticated :user do
