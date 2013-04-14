ee898b74456bf6e32050efcb238584aabed5027b
Pablo Marambio <pablo@pablomarambio.cl>
2013-04-14 16:16:59 -0300

    Imágenes y redes sociales en editar perfil

--- app/helpers/home_helper.rb
+++ app/helpers/home_helper.rb
@@ -1,2 +1,22 @@
 module HomeHelper
+	
+	def has_twitter?
+		has_social_network? "twitter"
+	end
+	def has_linkedin?
+		has_social_network? "linkedin"
+	end
+	def has_googleplus?
+		has_social_network? "google_oauth2"
+	end
+	def has_facebook?
+		has_social_network? "facebook"
+	end
+	def has_github?
+		has_social_network? "github"
+	end
+
+	def has_social_network? sn_name
+		@user.auth_providers.any? {|ap| ap.provider == sn_name}
+	end
 end
